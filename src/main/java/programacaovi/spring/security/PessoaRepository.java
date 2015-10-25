041cc189c6b5b90f4c641d7803b06441a59111e1
Rodrigo Habib Gregori <rodrigo.gregori@gmail.com>
2015-10-25 18:43:21 -0200

    Passo 2b: Adicionando Spring Data REST

--- src/main/java/programacaovi/spring/security/PessoaRepository.java
+++ src/main/java/programacaovi/spring/security/PessoaRepository.java
@@ -7,11 +7,13 @@ package programacaovi.spring.security;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  *
  * @author rodrigo
  */
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-    List<Pessoa> findByNomeLike(String nome);
+    // Adicionamos a tag @Param ao método para ser acessível via REST
+    List<Pessoa> findByNomeLike(@Param("nome") String nome);
 }
